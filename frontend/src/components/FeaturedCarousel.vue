da858a23b7142344cb443e0c8d4e950073abcb50
RifkiEr24 <rifkierlangga17@gmail.com>
2021-05-11 13:22:13 +0700

    add featured carousel on landing page

--- frontend/src/components/FeaturedCarousel.vue
+++ frontend/src/components/FeaturedCarousel.vue
@@ -13,17 +13,21 @@
       <div> <img class="mx-auto" src="@/assets/img/cloth5.png" alt=""></div>
       <div> <img class="mx-auto" src="@/assets/img/cloth6.png" alt=""></div>
       <template #prevArrow>
-      <div class="right-bot prev">
+      <div class="b">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
       </svg>
       </div>
     </template>
         <template #nextArrow>
-      <div class="right-bot next">
+      <div class="c">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
 </svg>
+      </div>
+    </template>
+       <template #customPaging>
+      <div class="carousel-dot">
       </div>
     </template>
     </VueSlickCarousel>
@@ -54,3 +58,58 @@ export default {
   },
 }
 </script>
+<style scoped lang="postcss">
+.slick-prev, .slick-next {
+   @apply rounded-lg;
+  @apply w-10;
+  @apply py-8;
+  @apply h-10;
+  @apply text-4xl;
+  @apply border;
+  @apply text-gray-400;
+  @apply border-gray-300 !important;
+  @apply hover:text-white;
+  @apply hover:bg-primary;
+  @apply hover:border-primary !important;
+  @apply transition;
+  @apply duration-300;
+  @apply focus:outline-none;
+  background-color: transparent;
+  text-align: center;
+  padding: 0;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid white;
+  cursor: pointer;
+  z-index: 3;
+}
+
+.slick-prev {
+  @apply ml-8;
+  }
+
+  .slick-next {
+    @apply mr-8
+  }
+
+  .slick-next:before{
+    content: '';
+  }
+    .slick-prev:before{
+    content: '';
+  }
+  .slick-active .carousel-dot{
+  @apply bg-primary;
+  @apply transition;
+  @apply duration-700;
+  @apply ease-in-out;
+  }
+  .carousel-dot{
+    @apply bg-gray-400;
+    @apply rounded-full;
+     @apply transition;
+  @apply duration-300;
+    width: 15px;
+    height: 15px;
+  }
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -31,7 +31,7 @@
     <div class="w-full md:w-7/12 p-5 relative">
     <p class="header text-6xl mt-0 md:mt-16">Humanize <br> The Virtual</p>
     <p class="w-full md:w-paragraph mt-10 font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
-    <button class="border-primary mt-12 border rounded-xl p-3 transition duration-300 hover:bg-primary hover:rounded-xl hover:text-white px-16">Let's Go</button>
+    <button class="border-primary mt-12 border rounded-xl p-3 transition duration-500 hover:bg-primary hover:rounded-xl hover:text-white px-16">Let's Go</button>
      <div class="absolute bottom-5 right-12 md:bottom-3 md:right-36 flex">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-7 -2 24 24" width="30" height="30" preserveAspectRatio="xMinYMin" class="icon__icon"><path d="M2.046 3.865v2.748H.032v3.36h2.014v9.986H6.18V9.974h2.775s.26-1.611.386-3.373H6.197V4.303c0-.343.45-.805.896-.805h2.254V0H6.283c-4.34 0-4.237 3.363-4.237 3.865z"></path></svg>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -4 24 24" width="30" height="30" preserveAspectRatio="xMinYMin" class="ml-3 icon__icon"><path d="M20 1.907a8.292 8.292 0 0 1-2.356.637A4.07 4.07 0 0 0 19.448.31a8.349 8.349 0 0 1-2.607.98A4.12 4.12 0 0 0 13.846.015c-2.266 0-4.103 1.81-4.103 4.04 0 .316.036.625.106.92A11.708 11.708 0 0 1 1.393.754a3.964 3.964 0 0 0-.554 2.03c0 1.403.724 2.64 1.824 3.363A4.151 4.151 0 0 1 .805 5.64v.05c0 1.958 1.415 3.591 3.29 3.963a4.216 4.216 0 0 1-1.08.141c-.265 0-.522-.025-.773-.075a4.098 4.098 0 0 0 3.832 2.807 8.312 8.312 0 0 1-5.095 1.727c-.332 0-.658-.02-.979-.056a11.727 11.727 0 0 0 6.289 1.818c7.547 0 11.673-6.157 11.673-11.496l-.014-.523A8.126 8.126 0 0 0 20 1.907z"></path></svg>
@@ -49,7 +49,7 @@
     <featured-carousel/>
   </div>
   <div class="w-full" v-else-if="featured == false">
-    c    
+    <featured-carousel/>   
   </div>
     </transition>
 
