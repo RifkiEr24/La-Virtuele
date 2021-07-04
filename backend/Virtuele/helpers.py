6108f7b84aa839fa325e7d6c759d8d547d0002f2
harizMunawar <munawarhariz@gmail.com>
2021-07-04 15:20:55 +0700

    Create category crud test case, and factory for product and category

--- backend/Virtuele/helpers.py
+++ backend/Virtuele/helpers.py
@@ -1,9 +1,13 @@
+from product.models import Category, Product
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from random import randint
+
 class VirtueleTestBase(TestCase):
+    random_word_list = ['apostils', 'estivate', 'rioted', 'doze', 'lexicalizing', 'driftier', 'reinjection', 'musician', 'endosperms', 'cummerbunds', 'masculinizing', 'fabbest', 'semicolonialism', 'fabulous', 'clearstory', 'rared', 'lawmaking', 'confronts', 'conquians', 'morulae', 'pinto', 'dropkicker', 'antisex', 'euryokous', 'outyell', 'reinvigorations', 'brainstormers', 'ogrish', 'grails', 'heaume', 'apollos', 'morselling', 'gausses', 'exostoses', 'degreed', 'castellans', 'gridlocking', 'twirling', 'ordures', 'glum', 'capitulate', 'skill', 'brigandines', 'hustles', 'monolayers', 'forceless', 'felsic', 'procurator', 'fetas', 'conventionalist', 'bitchier', 'hypothecators', 'sniffishnesses', 'resembling', 'wastefully', 'audaciousnesses', 'handfasting', 'woodnotes', 'checkreins', 'corduroys', 'airstrip', 'torturing', 'testify', 'frenziedly', 'iguanian', 'gluten', 'opuntia', 'renitent', 'caprocks', 'nonenergy', 'centralities', 'inamoratas', 'mischanneled', 'morale', 'psychologises', 'abridgment', 'cerebrating', 'tautness', 'stigmatizes', 'endothecium', 'doux', 'contusing', 'dystrophy', 'desirableness', 'hewers', 'putschists', 'financiered', 'roturiers', 'emotionalizes', 'stonewaller', 'measles', 'chertier', 'lignites', 'cosmopolitism', 'bridesmaids', 'sashing', 'denouncements', 'intellect', 'prototyping', 'sociologese']
     def user_admin_factory(self):
         get_user_model().objects.create_user(email='admin@admin.com',
                                              password='admin',
@@ -17,6 +21,26 @@ class VirtueleTestBase(TestCase):
                                              first_name='user',
                                              username='punyUser',
                                              is_active=True)
+
+    def product_factory(self, n=5, category: list=None):
+        if not category:
+            category = [Category.objects.create(name='Placeholder Category').id]
+
+        category_count = len(category)
+
+        for i in range(n):
+            p = Product.objects.create(name=' '.join([self.random_word_list[randint(0, 99)] for i in range(2)]).title(),
+                                       description=' '.join([self.random_word_list[randint(0, 99)] for i in range(10)]).capitalize(),
+                                       price=randint(1000, 1000000),
+                                       material=' '.join([self.random_word_list[randint(0, 99)] for i in range(2)]).upper(),
+                                       is_featured=(0 == i%2))
+            p.category.add(Category.objects.get(id=randint(1, category_count)))
+            p.save()
+
+    def category_factory(self, n=5):
+        for i in range(n):
+            Category.objects.create(name=' '.join([self.random_word_list[randint(0, 99)] for i in range(2)]).title())
+
     @property
     def admin_jwt(self):
         user = get_user_model().objects.get(username='adminGod')
--- backend/product/tests.py
+++ backend/product/tests.py
@@ -4,17 +4,14 @@ from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
 from product.models import Category, Product
-from product.serializers import ProductSerializer
+from product.serializers import CategorySerializer, ProductSerializer
 from Virtuele.helpers import VirtueleTestBase
 
 class ProductCRUD(VirtueleTestBase):
     def setUp(self):
-        for i in range (10):
-            Product.objects.create( name=f'Product Test {i}',
-                                    description=f'This is just a sample {i}.',
-                                    price=int(f'{i}00000'),
-                                    material='Cotton Candy')
-        Category.objects.create(name='Cotton 200gsm')
+        self.user_admin_factory()
+        self.product_factory()
+        self.client = APIClient()
 
         self.valid_payload = {
             'name': 'Collapse',
@@ -31,44 +28,61 @@ class ProductCRUD(VirtueleTestBase):
             'price': 200000,
             'material': 'Cotton 200gsm'
         }
-        
-        self.user_admin_factory()
-        self.client = APIClient()
 
     def test_get_all_products(self):
-        response = self.client.get(reverse('product-list'))
+        response = self.client.get('/api/v1/products/')
         products = ProductSerializer(Product.objects.all(), many=True)
         self.assertEqual(response.data, products.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_a_valid_product(self):
-        response = self.client.post(reverse('product-list'),
+        response = self.client.get('/api/v1/products/?featured=true')
+        products = ProductSerializer(Product.objects.filter(is_featured=True), many=True)
+        self.assertEqual(response.data, products.data)
+        if not products: self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.client.get('/api/v1/products/?category=1')
+        products = ProductSerializer(Product.objects.filter(category=1), many=True)
+        self.assertEqual(response.data, products.data)
+        if not products: self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_create_a_product(self):
+        response = self.client.post('/api/v1/products/',
                                     json.dumps(self.valid_payload),
                                     content_type='application/json',
                                     **self.user_jwt)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-        response = self.client.post(reverse('product-list'),
+        response = self.client.post('/api/v1/products/',
                                     json.dumps(self.valid_payload),
                                     content_type='application/json',
                                     **self.admin_jwt)
+        product_1_slug = response.data['slug']
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
-    def test_create_an_invalid_product(self):
-        response = self.client.post(reverse('product-list'),
+        
+        response = self.client.post('/api/v1/products/',
+                                    json.dumps(self.valid_payload),
+                                    content_type='application/json',
+                                    **self.admin_jwt)
+        product_2_slug = response.data['slug']
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertNotEqual(product_1_slug, product_2_slug)
+
+        response = self.client.post('/api/v1/products/',
                                     json.dumps(self.invalid_payload),
                                     content_type='application/json',
                                     **self.user_jwt)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-        response = self.client.post(reverse('product-list'),
+        response = self.client.post('/api/v1/products/',
                                     json.dumps(self.invalid_payload),
                                     content_type='application/json',
                                     **self.admin_jwt)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_get_single_product(self):
-        response = self.client.get('/api/v1/products/product-test-1/')
+        product = Product.objects.get(id=1)
+
+        response = self.client.get(f'/api/v1/products/{product.slug}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         response = self.client.get('/api/v1/products/invalid-slug-lmao/')
@@ -79,18 +93,106 @@ class ProductCRUD(VirtueleTestBase):
             'name': 'Updated Product Heehe',
             'category': [1]
         }
+        product = Product.objects.get(id=1)
 
-        response = self.client.put('/api/v1/products/product-test-1/',
+        response = self.client.put(f'/api/v1/products/{product.slug}/',
                                    updated_payload,
                                    format='json',
                                    **self.user_jwt)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-        response = self.client.put('/api/v1/products/product-test-1/',
+        response = self.client.put(f'/api/v1/products/{product.slug}/',
                                    updated_payload,
                                    format='json',
                                    **self.admin_jwt)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['slug'], 'updated-product-heehe')
+        self.assertEqual(response.data, ProductSerializer(Product.objects.get(slug='updated-product-heehe')).data)
 
+        response = self.client.put(f'/api/v1/products/{product.slug}/',
+                                   updated_payload,
+                                   format='json',
+                                   **self.admin_jwt)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_delete_single_product(self):
+        product = Product.objects.get(id=1)
+
+        response = self.client.delete(f'/api/v1/products/{product.slug}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        response = self.client.delete(f'/api/v1/products/{product.slug}/', **self.user_jwt)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.delete(f'/api/v1/products/invalidslug/', **self.admin_jwt)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        response = self.client.delete(f'/api/v1/products/{product.slug}/', **self.admin_jwt)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+class CategoryCRUD(VirtueleTestBase):
+    def setUp(self):
+        self.user_admin_factory()
+        self.client = APIClient()
+        self.category_factory(n=3)
+    
+    def test_get_all_categories(self):
+        response = self.client.get('/api/v1/categories/')
+        categories = CategorySerializer(Category.objects.all(), many=True)
+        self.assertEqual(response.data, categories.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_a_category(self):
+        response = self.client.post('/api/v1/categories/',
+                                    {'name': 'New Category'},
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        response = self.client.post('/api/v1/categories/',
+                                    {'name': 'New Category'},
+                                    format='json',
+                                    **self.user_jwt)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.post('/api/v1/categories/',
+                                    {'name': 'New Category'},
+                                    format='json',
+                                    **self.admin_jwt)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post('/api/v1/categories/',
+                                    {'name': ''},
+                                    format='json',
+                                    **self.admin_jwt)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_single_category(self):
+        response = self.client.get('/api/v1/categories/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.get('/api/v1/categories/invalidid/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_single_category(self):
+        response = self.client.put('/api/v1/categories/1/',
+                                   {'name': 'New Name'},
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        response = self.client.put('/api/v1/categories/1/',
+                                   {'name': 'New Name'},
+                                   format='json',
+                                   **self.user_jwt)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.put('/api/v1/categories/1/',
+                                   {'name': ''},
+                                   format='json',
+                                   **self.admin_jwt)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.put('/api/v1/categories/1/',
+                                   {'name': 'New Name'},
+                                   format='json',
+                                   **self.admin_jwt)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'New Name')
