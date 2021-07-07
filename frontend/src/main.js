59f81a3f6ac16624df1e3da94a9def42df0f1ac5
RifkiEr24 <rifkierlangga17@gmail.com>
2021-07-08 02:44:10 +0700

    add vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,26 +10,30 @@ import VueAxios from 'vue-axios';
 
 import axios from 'axios'
 
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-  // optional style for arrows & dots
-  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 
-  Vue.directive('scroll', {
-    inserted: function (el, binding) {
-      let f = function (evt) {
-        if (binding.value(evt, el)) {
-          window.removeEventListener('scroll', f)
-        }
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+import store from './store'
+
+Vue.directive('scroll', {
+  inserted: function (el, binding) {
+    let f = function (evt) {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('scroll', f)
       }
-      window.addEventListener('scroll', f)
     }
-  })
-  
+    window.addEventListener('scroll', f)
+  }
+})
+
 Vue.use(Transitions)
 Vue.use(VuePageTransition)
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 new Vue({
   router,
-  render: (h) => h(App),
+  store,
+  render: (h) => h(App)
 }).$mount('#app');
